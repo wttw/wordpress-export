f5ecabc6eedf24f487449eff925fec94d3cd9e62
Steve Atkins <steve@wordtothewise.com>
2025-01-02 11:35:36 +0000

    Add caching to the http client, option to mirror images, fetch comments.

--- go.mod
+++ go.mod
@@ -1,11 +1,14 @@
 module github.com/wttw/wordpress-export
 
-go 1.14
+go 1.23
 
 require (
+	github.com/google/btree v1.1.3 // indirect
 	github.com/gookit/color v1.2.5
+	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79 // indirect
 	github.com/mitchellh/mapstructure v1.3.2
+	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
 	github.com/spf13/pflag v1.0.5
-	golang.org/x/net v0.0.0-20200625001655-4c5254603344
+	golang.org/x/net v0.33.0
 	gopkg.in/yaml.v2 v2.3.0
 )
--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"bytes"
+	"crypto/md5"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"golang.org/x/net/publicsuffix"
 	"io"
+	"mime"
 	"net/http"
 	"net/url"
 	"os"
@@ -37,9 +41,16 @@ var postFilename string
 var frontmatterFile string
 var showHelp bool
 var showVersion bool
+var filter string
+var cacheDir string
+var stale bool
+var mirror bool
+var userAgent string
 
 const myName = "wordpress-export"
-const version = "0.1"
+const version = "0.2"
+
+var client *http.Client
 
 func init() {
 	flag.BoolVar(&saveMeta, "meta", false, "save tags, categories and authors")
@@ -51,19 +62,42 @@ func init() {
 	flag.BoolVarP(&quiet, "quiet", "q", false, "Don't print progress")
 	flag.BoolVar(&silent, "silent", false, "Don't print progress or warnings")
 	flag.IntVar(&sample, "sample", 0, "Only retrieve this many posts")
+	flag.StringVar(&filter, "filter", "", "Only retrieve posts with urls containing this regexp")
 	flag.StringVar(&postFilename, "postfile", "index.md", "The filename for each post")
 	flag.StringVar(&frontmatterFile, "frontmatter", "", "Read additional frontmatter from this file")
-
+	flag.StringVar(&cacheDir, "cache", "", "Cache directory")
+	flag.BoolVar(&stale, "stale", false, "Do not expire cached results")
+	flag.BoolVar(&mirror, "mirror", false, "Mirror remote images")
+	flag.StringVar(&userAgent, "user-agent", "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0", "Override request user-agent")
 	flag.BoolVarP(&showHelp, "help", "h", false, "Show this help")
 	flag.BoolVarP(&showVersion, "version", "V", false, "Show version")
 
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s [flags] <your blog url>\n", myName)
+		_, _ = fmt.Fprintf(os.Stderr, "Usage: %s [flags] <your blog url>\n", myName)
 		flag.PrintDefaults()
 	}
 }
 
 var logWriter io.Writer
+
+type Missing struct {
+	Page   string
+	URL    string
+	Status string
+}
+
+type Warning struct {
+	Page    string
+	Message string
+}
+
+type Errors struct {
+	Missing  []Missing
+	Warnings []Warning
+}
+
+var errorList Errors
+var currentPage string
 
 func main() {
 	flag.Parse()
@@ -72,7 +106,7 @@ func main() {
 		os.Exit(0)
 	}
 	if showVersion {
-		fmt.Fprintf(os.Stderr, "%s version %s\n", myName, version)
+		_, _ = fmt.Fprintf(os.Stderr, "%s version %s\n", myName, version)
 		os.Exit(0)
 	}
 
@@ -87,6 +121,14 @@ func main() {
 	}
 
 	quiet = quiet || silent
+
+	client = &http.Client{
+		Timeout: time.Second * 30,
+	}
+
+	if cacheDir != "" {
+		_ = os.MkdirAll(cacheDir, 0755)
+	}
 
 	// Handle the parameter, which we hope is "a link to the site"
 	switch flag.NArg() {
@@ -125,20 +167,30 @@ func main() {
 		frontmatter = strings.TrimSpace(buff.String()) + "\n"
 	}
 
+	filterRe, err := regexp.Compile(filter)
+	if err != nil {
+		fatal("Failed to compile filter: %v", err)
+	}
+
 	_ = os.MkdirAll(dest, 0755)
 
 	info("Using API at %s", apiUrl)
 	users := getUsers()
 	categories := getCategories()
 	tags := getTags()
+	comments := getComments()
 	if saveMeta {
 		writeMeta("users", &users)
 		writeMeta("categories", &categories)
 		writeMeta("tags", &tags)
+		writeMeta("comments", &comments)
 	}
 	posts := getPosts()
 
 	for _, p := range posts {
+		if !filterRe.MatchString(p.Link) {
+			continue
+		}
 		author, ok := users[p.Author]
 		if !ok {
 			fatal("No such author as %d in post %s", p.Author, p.Link)
@@ -165,8 +217,35 @@ func main() {
 		}
 		p.TagNames = tagNames
 		savePost(p, frontmatter)
+		cm, ok := comments[p.ID]
+		if ok {
+			postPath := postDirectory(p)
+
+			// Where do we write the output for this post?
+			outputDir := filepath.Join(append([]string{dest}, postPath...)...)
+			commentFile, err := os.Create(filepath.Join(outputDir, "comments.json"))
+			if err != nil {
+				fatal("Failed to create comments file: %v", err)
+			}
+			enc := json.NewEncoder(commentFile)
+			enc.SetIndent("", "  ")
+			enc.SetEscapeHTML(false)
+			err = enc.Encode(cm)
+			if err != nil {
+				fatal("Failed to encode comments: %v", err)
+			}
+		}
 	}
 	status("Saved all posts")
+	if len(errorList.Missing) > 0 {
+		warn("There were %d missing assets", len(errorList.Missing))
+	}
+	if len(errorList.Warnings) > 0 {
+		warn("There were %d warnings", len(errorList.Warnings))
+	}
+	if len(errorList.Missing) > 0 || len(errorList.Warnings) > 0 {
+		writeMeta("errors", errorList)
+	}
 }
 
 type ResultPost struct {
@@ -212,6 +291,7 @@ func postDirectory(p Post) []string {
 }
 
 func savePost(p Post, frontmatter string) {
+	currentPage = p.Link
 	sourceUrl, err := url.Parse(p.Link)
 	if err != nil {
 		fatal("Failed to parse post url '%s': %v", p.Link, err)
@@ -294,6 +374,37 @@ type Asset struct {
 	Filename string
 }
 
+func asset(url *url.URL, filename string) *Asset {
+	a, err := head(url.String())
+	if err == nil && a.StatusCode >= 200 && a.StatusCode <= 299 {
+		suffixes, err := mime.ExtensionsByType(a.ContentType)
+		if err == nil && len(suffixes) > 0 {
+			for _, suffix := range suffixes {
+				if strings.HasSuffix(filename, suffix) {
+					return &Asset{
+						Url:      url,
+						Filename: filename,
+					}
+				}
+			}
+			suffix := suffixes[0]
+			justType, _, _ := mime.ParseMediaType(a.ContentType)
+			switch justType {
+			case "text/html":
+				suffix = ".html"
+			case "image/jpeg":
+				suffix = ".jpg"
+			default:
+			}
+			filename = filename + suffix
+		}
+	}
+	return &Asset{
+		Url:      url,
+		Filename: filename,
+	}
+}
+
 // Return a remote URL and local filename for assets that we want
 // to copy. We assume that any asset on the same hostname is worth
 // considering as a local asset or page.
@@ -303,14 +414,36 @@ func localAsset(assetUrl string, sourceUrl *url.URL) *Asset {
 		warn("Failed to parse asset url '%s': %v", assetUrl, err)
 		return nil
 	}
+	// Only mirror local assets
+	if !strings.HasPrefix(strings.ToLower(au.Path), wpUploads) {
+		return nil
+	}
 
 	refUrl := sourceUrl.ResolveReference(au)
 	assetFilename := path.Base(refUrl.Path)
-	if strings.ToLower(sourceUrl.Host) == strings.ToLower(refUrl.Host) {
-		return &Asset{
-			Url:      refUrl,
-			Filename: assetFilename,
-		}
+	sourceOrg, sourceErr := publicsuffix.EffectiveTLDPlusOne(sourceUrl.Hostname())
+	refOrg, refErr := publicsuffix.EffectiveTLDPlusOne(refUrl.Hostname())
+	if sourceErr == nil && refErr == nil && sourceOrg == refOrg {
+		return asset(refUrl, assetFilename)
+	}
+	if strings.ToLower(sourceUrl.Hostname()) == strings.ToLower(refUrl.Hostname()) {
+		return asset(refUrl, assetFilename)
+	}
+	return nil
+}
+
+func copyImage(assetUrl string, sourceUrl *url.URL) *Asset {
+	au, err := url.Parse(strings.TrimSpace(assetUrl))
+	if err != nil {
+		warn("Failed to parse asset url '%s': %v", assetUrl, err)
+		return nil
+	}
+
+	refUrl := sourceUrl.ResolveReference(au)
+	assetFilename := path.Base(refUrl.Path)
+
+	if mirror || strings.ToLower(sourceUrl.Hostname()) == strings.ToLower(refUrl.Hostname()) {
+		return asset(refUrl, assetFilename)
 	}
 	return nil
 }
@@ -318,20 +451,25 @@ func localAsset(assetUrl string, sourceUrl *url.URL) *Asset {
 var plausibleSuffixRe = regexp.MustCompile(`\.(png|jpg|gif|pdf|jpeg|webp)$`)
 
 func fetchAsset(asset *Asset, dir string) string {
-	if !strings.HasPrefix(strings.ToLower(asset.Url.Path), wpUploads) {
+	if !mirror && !strings.HasPrefix(strings.ToLower(asset.Url.Path), wpUploads) {
 		// internal link to a page, so don't mirror it
 		return asset.Url.Path
 	}
 	if !plausibleSuffixRe.MatchString(asset.Filename) {
 		warn("Suspicious filename: %s", asset.Filename)
 	}
-	resp, err := http.Get(asset.Url.String())
+	resp, err := get(asset.Url.String())
 	if err != nil {
 		warn("Failed to get linked file %s: %v", asset.Url, err)
 		return asset.Url.String()
 	} else {
 		if resp.StatusCode != 200 {
-			warn("Non-200 response fetching file: %s (%s)", asset.Url, resp.Status)
+			errorList.Missing = append(errorList.Missing, Missing{
+				Page:   dir,
+				URL:    asset.Url.String(),
+				Status: resp.Status,
+			})
+			//warn("Non-200 response fetching file: %s (%s)", asset.Url, resp.Status)
 			return asset.Url.String()
 		} else {
 			of, err := os.Create(filepath.Join(dir, asset.Filename))
@@ -373,7 +511,7 @@ func fixImages(node *html.Node, dir string, sourceUrl *url.URL) {
 	if node.Type == html.ElementNode && node.Data == "img" {
 		for i, attr := range node.Attr {
 			if attr.Key == "src" {
-				asset := localAsset(attr.Val, sourceUrl)
+				asset := copyImage(attr.Val, sourceUrl)
 				if asset != nil {
 					// Fetch image to same directory as post and fix src to point to it.
 					node.Attr[i] = html.Attribute{
@@ -391,7 +529,7 @@ func fixImages(node *html.Node, dir string, sourceUrl *url.URL) {
 					if len(fields) != 2 {
 						genParts = append(genParts, part)
 					} else {
-						asset := localAsset(fields[0], sourceUrl)
+						asset := copyImage(fields[0], sourceUrl)
 						if asset != nil {
 							fields[0] = fetchAsset(asset, dir)
 						}
@@ -491,6 +629,35 @@ func getUsers() map[int]*User {
 	return rm
 }
 
+type Comment struct {
+	ID               int
+	Author           int               `json:"author,omitempty" mapstructure:"author,omitempty"`
+	AuthorEmail      string            `json:"author_email,omitempty" mapstructure:"author_email,omitempty"`
+	AuthorIP         string            `json:"author_ip,omitempty" mapstructure:"author_ip,omitempty"`
+	AuthorName       string            `json:"author_name,omitempty" mapstructure:"author_name,omitempty"`
+	AuthorURL        string            `json:"author_url,omitempty" mapstructure:"author_url,omitempty"`
+	AuthorUserAgent  string            `json:"author_user_agent,omitempty" mapstructure:"author_user_agent,omitempty"`
+	Content          Rendered          `json:"content,omitempty" mapstructure:"content,omitempty"`
+	Date             string            `json:"date,omitempty" mapstructure:"date,omitempty"`
+	DateGMT          string            `json:"date_gmt,omitempty" mapstructure:"date_gmt,omitempty"`
+	Link             string            `json:"link,omitempty" mapstructure:"link,omitempty"`
+	Parent           int               `json:"parent,omitempty" mapstructure:"parent,omitempty"`
+	Post             int               `json:"post,omitempty" mapstructure:"post,omitempty"`
+	Type             string            `json:"type,omitempty" mapstructure:"type,omitempty"`
+	AuthorAvatarURLs map[string]string `json:"author_avatar_urls,omitempty" mapstructure:"author_avatar_urls,omitempty"`
+	Meta             []any             `json:"meta,omitempty" mapstructure:"meta,omitempty"`
+}
+
+func getComments() map[int][]Comment {
+	result := []Comment{}
+	fetch("comments", &result, "comments?context=view&_fields=id,author,author_email,author_ip,author_name,author_url,author_user_agent,content,date,date_gmt,link,parent,post,type,author_avatar_urls,meta")
+	ret := map[int][]Comment{}
+	for _, r := range result {
+		ret[r.Post] = append(ret[r.Post], r)
+	}
+	return ret
+}
+
 type Rendered struct {
 	Rendered string
 }
@@ -513,7 +680,7 @@ type Post struct {
 	TagNames      []string
 }
 
-// Fetch all the Wordpress posts
+// Fetch all the WordPress posts
 func getPosts() []Post {
 	result := []Post{}
 	fetch("posts", &result, "posts?context=view&_fields=id,date_gmt,slug,status,title,content,excerpt,author,categories,tags,link")
@@ -527,6 +694,91 @@ func getPosts() []Post {
 		rm[r.ID] = struct{}{}
 	}
 	return result
+}
+
+type Response struct {
+	Request     string
+	StatusCode  int
+	Status      string
+	BodyContent []byte
+	Body        *bytes.Reader `json:"-"`
+	ContentType string
+	Error       string
+}
+
+// get does an http.Get with a local cache
+func get(u string) (Response, error) {
+	var key string
+	if cacheDir != "" {
+		key = fmt.Sprintf("%16x", md5.Sum([]byte(u)))
+		// fmt.Printf("%s -> %s\n", key, u)
+		f, err := os.Open(filepath.Join(cacheDir, key))
+		if err == nil {
+			var resp Response
+			dec := json.NewDecoder(f)
+			err = dec.Decode(&resp)
+			if err == nil {
+				if resp.Error != "" {
+					return Response{}, errors.New(resp.Error)
+				}
+				resp.Body = bytes.NewReader(resp.BodyContent)
+				return resp, nil
+			}
+			warn("error decoding cached response for %s: %v", u, err)
+		}
+	}
+	req, err := http.NewRequest(http.MethodGet, u, nil)
+	if err != nil {
+		return Response{}, err
+	}
+	if userAgent != "" {
+		req.Header.Set("User-Agent", userAgent)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		cacheResponse(key, Response{
+			Request: u,
+			Error:   err.Error(),
+		})
+		return Response{}, err
+	}
+	body, err := io.ReadAll(resp.Body)
+	// fmt.Printf("body: %s\n", string(body))
+	if err != nil {
+		return Response{}, err
+	}
+	_ = resp.Body.Close()
+	r := Response{
+		Request:     u,
+		StatusCode:  resp.StatusCode,
+		Status:      resp.Status,
+		BodyContent: body,
+		ContentType: resp.Header.Get("Content-Type"),
+		Body:        bytes.NewReader(body),
+	}
+	cacheResponse(key, r)
+	return r, nil
+}
+
+func cacheResponse(key string, r Response) {
+	if cacheDir != "" {
+		f, err := os.Create(filepath.Join(cacheDir, key))
+		if err == nil {
+			enc := json.NewEncoder(f)
+			enc.SetEscapeHTML(false)
+			err = enc.Encode(r)
+			if err != nil {
+				fatal("failed to cache response for %s: %v", r.Request, err)
+			}
+		} else {
+			fatal("failed to cache response for %s: %v", r.Request, err)
+		}
+	}
+}
+
+func head(u string) (Response, error) {
+	// If we're not caching results we could just a HEAD here rather than GET
+	return get(u)
 }
 
 // Fetch a result set from WordPress, unmarshall it to our result
@@ -548,9 +800,6 @@ func getAll(u *url.URL, name string) []interface{} {
 	if sample > 0 && name == "posts" {
 		limit = sample
 	}
-	client := http.Client{
-		Timeout: 30 * time.Second,
-	}
 	ret := []interface{}{}
 	page := 1
 	pageSize := 100
@@ -564,7 +813,7 @@ func getAll(u *url.URL, name string) []interface{} {
 		u.RawQuery = q.Encode()
 
 		status("fetching %s %d ...", name, (page-1)*pageSize)
-		res, err := client.Get(u.String())
+		res, err := get(u.String())
 		if err != nil {
 			fatal("failed to fetch %s: %v", u, err)
 		}
@@ -575,7 +824,6 @@ func getAll(u *url.URL, name string) []interface{} {
 		if err != nil {
 			fatal("failed to parse response from %s: %v", u, err)
 		}
-		res.Body.Close()
 		ret = append(ret, thisPage...)
 		if len(thisPage) < pageSize || len(ret) >= limit {
 			endStatus("fetched %d %s", len(ret), name)
@@ -626,6 +874,10 @@ func info(format string, a ...interface{}) {
 
 func warn(format string, a ...interface{}) {
 	msg := fmt.Sprintf(format, a...) + "\n"
+	errorList.Warnings = append(errorList.Warnings, Warning{
+		Page:    currentPage,
+		Message: msg,
+	})
 	if logWriter != nil {
 		_, _ = io.WriteString(logWriter, "WARN: "+msg)
 	}
@@ -669,9 +921,6 @@ func parseURL(rawurl string) (*url.URL, error) {
 // findApi does discovers the API fo a wordpress site, as documented at
 // https://developer.wordpress.org/rest-api/using-the-rest-api/discovery/
 func findApi(siteUrl *url.URL) string {
-	client := http.Client{
-		Timeout: 30 * time.Second,
-	}
 	head, err := client.Head(siteUrl.String())
 	if err != nil {
 		fatal("Couldn't fetch %s while looking for site API: %v", siteUrl, err)
